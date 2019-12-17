365383b1b679bab67add3045d6623183535abf90
szidonia.laszlo <szidonia.laszlo@directtwo.hu>
2019-12-17 16:22:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/szidonialaszlo/createdbproject3/CreateDB.java
@@ -0,0 +1,13 @@
+package com.example.szidonialaszlo.createdbproject3;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by szidonia.laszlo on 2017. 11. 16..
+ */
+
+public class CreateDB extends SQLiteDatabase {
+
+    CreateDB() {
+    }
+}
